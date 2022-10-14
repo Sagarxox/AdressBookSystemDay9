1c990ce57096b1a64182c5633d03be8190df2b10
sagar <skatahre@gmail.com>
2022-10-14 08:11:49 +0530

    create contact in address

---
+++ Person.cs
@@ -0,0 +1,39 @@
+﻿namespace A
+{
+
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string Email { get; set; }
+
+        public Person(string firstName, string lastName, string phoneNumber, string address, string city, string state, string zipCode, string email)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.PhoneNumber = phoneNumber;
+            this.Address = address;
+            this.City = city;
+            this.State = state;
+            this.ZipCode = zipCode;
+            this.Email = email;
+
+        }
+
+        public string Getinfo()
+        {
+            string message = "FirstName :" + FirstName + "LastName :" + LastName + "Phone number :" + PhoneNumber + "Address :" + Address + "City :" + City + "State :" + State + "Zip code : " + ZipCode + "Email :" + Email;
+
+        
+)
+        
+            return string.Format(firstName, lastName, phoneNumber, address, city, state, zipCode, email);
+        }
+    }
+}
