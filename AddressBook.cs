8241bfafaa2b2dc858ab0ac6f601d794c7c48fac
sagar <skatahre@gmail.com>
2022-10-16 16:04:01 +0530

    edit contact

--- AddressBook.cs
+++ AddressBook.cs
@@ -19,9 +19,55 @@ namespace AddressBookSystem
         {
             foreach (var contact in list)
             {
-                Console.WriteLine("FirstName: " + contact.FirstName + "\n" + "LastName: " + contact.LastName + "\n" + "Address: " + contact.Address + "\n" + "City: " + contact.City + "\n" + "State: " + contact.State + "\n" + "Zipcode: " + contact.Zip + "\n" + "Phone Number: " + contact.Phoneno + "\n" + "Email: " + contact.Email);
+                Console.WriteLine("FirstName: " + contact.FirstName + "\n" + "LastName: " + contact.LastName + "\n" + "Address: " + contact.Address + "\n" + "City: " + contact.City + "\n" + "State: " + contact.State + "\n" + "Zipcode : " + contact.ZipCode + "\n" + "Phone Number: " + contact.PhoneNumber + "\n" + "Email: " + contact.Email);
             }
         }
-      
+        public void EditContact(string name)
+        {
+            foreach (var contact in list)
+            {
+                if (contact.FirstName == name)
+                {
+                    Console.WriteLine("-----------------------------\n");
+                    Console.WriteLine("Enter The Option Which You Can Edit : ");
+                    Console.WriteLine("1.Lastname\n2.Address\n3.City\n4.State\n5.Zip\n6.Phoneno\n7.Email");
+                    int option = Convert.ToInt32(Console.ReadLine());
+                    switch (option)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter LastName to Edit");
+                            contact.LastName = Console.ReadLine();
+                            break;
+                        case 2:
+                            Console.WriteLine("Enter Address to Edit");
+                            contact.Address = Console.ReadLine();
+                            break;
+                        case 3:
+                            Console.WriteLine("Enter City to Edit");
+                            contact.City = Console.ReadLine();
+                            break;
+                        case 4:
+                            Console.WriteLine("Enter State to Edit");
+                            contact.State = Console.ReadLine();
+                            break;
+                        case 5:
+                            Console.WriteLine("Enter Zip code to Edit");
+                            contact.ZipCode = (Console.ReadLine());
+                            break;
+                        case 6:
+                            Console.WriteLine("Enter PhoneNumbrer to Edit");
+                            contact.PhoneNumber = Console.ReadLine();
+                            break;
+                        case 7:
+                            Console.WriteLine("Enter Email to Edit");
+                            contact.Email = Console.ReadLine();
+                            break;
+                    }
+                    Console.WriteLine("------- Edited Contact  List --------");
+                    Display();
+                }
+            }
+        }
+
     }
 }
